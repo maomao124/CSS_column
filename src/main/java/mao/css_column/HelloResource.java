1cafb3566f18b564538b91cb8a6a8ce6317ca4fa
毛 <1296193245@qq.com>
2022-02-18 18:31:39 +0800

    CSS_column

---
+++ src/main/java/mao/css_column/HelloResource.java
@@ -0,0 +1,16 @@
+package mao.css_column;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/hello-world")
+public class HelloResource
+{
+    @GET
+    @Produces("text/plain")
+    public String hello()
+    {
+        return "Hello, World!";
+    }
+}
